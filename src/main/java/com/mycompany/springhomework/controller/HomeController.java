a799d316f2906539022c3797ff9747a302e10504
hyeonjuleebaek <hyeonjuleebaek@gmail.com>
2023-07-19 22:58:00 +0900

    Ch02Controller 수정

--- src/main/java/com/mycompany/springhomework/controller/HomeController.java
+++ src/main/java/com/mycompany/springhomework/controller/HomeController.java
@@ -1,5 +1,8 @@
 package com.mycompany.springhomework.controller;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,12 +11,31 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Controller
 public class HomeController {
+	//private static final Logger logger = LoggerFactory.getLogger(Ch01Controller.class);
+	
+	public HomeController() {
+		log.info("실행");
+	}
+	
+	@PostConstruct
+	public void method1() {
+		log.info("실행");
+	}
+	
+	@PostConstruct
+	public void method2() {
+		log.info("실행");
+	}
+	
+	@PreDestroy
+	public void method3() {
+		log.info("실행");
+	}
+	
 	@RequestMapping("/")
 	public String index() {
-		log.info("실행1");
-		log.info("실행2");
-		log.info("실행3");
-		log.info("실행4");
-		return "index";
+		//logger.info("실행");
+		log.info("실행");
+		return "home";
 	}
 }
